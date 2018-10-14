2ffdd3d26760ad68a67fc9c07159022b041a4da2
Egor Belkov <Subgorn@gmail.com>
2018-10-14 20:15:10 +0700

    Heroku config change

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS skytecgames;
-CREATE DATABASE skytecgames;
-USE skytecgames;
+DROP DATABASE IF EXISTS oa4wm3rd1n7vrr3l;
+CREATE DATABASE oa4wm3rd1n7vrr3l;
+USE oa4wm3rd1n7vrr3l;
 CREATE TABLE player(
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(255) NOT NULL,
