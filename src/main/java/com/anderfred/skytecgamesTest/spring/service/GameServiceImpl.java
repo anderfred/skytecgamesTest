e69645c719f40c290424047a14ec197fb263ad78
Egor Belkov <Subgorn@gmail.com>
2018-10-14 20:01:35 +0700

    Add footer stats, test db changes

--- src/main/java/com/anderfred/skytecgamesTest/spring/service/GameServiceImpl.java
+++ src/main/java/com/anderfred/skytecgamesTest/spring/service/GameServiceImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.persistence.EntityManager;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO player(name, password, health, damage, rating, ready)
-VALUES ("fredx", "123", 100, 10, 50, 0);
+VALUES ("test", "test", 100, 10, 50, 0);
